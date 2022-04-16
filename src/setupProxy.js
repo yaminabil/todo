296456dae24038ab587a80813988fe4309bb0216
Bilal Younes <bilal94.younes@gmail.com>
2022-04-16 04:24:10 -0400

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(["/api/todo"], {
-      target: "http://localhost:3001",
-    })
+    createProxyMiddleware("/api/**", { target: "http://localhost:3001" })
   );
 };
