9c8c3df334fb3d324e326106958d4f99ec73622a
Bilal Younes <bilal94.younes@gmail.com>
2022-04-16 04:15:44 -0400

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], {
+    createProxyMiddleware(["/api/todo"], {
       target: "http://localhost:3001",
     })
   );
